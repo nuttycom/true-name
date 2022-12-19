6ec32f4170a4bf823f6d80d1e3b6dc3e18746d87
Kris Nuttycombe <kris@nutty.land>
2022-12-18 20:54:53 -0700

    Add a flake.nix build configuration.

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Template Haskell hack to violate module abstractions";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/haskell-updates";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: let
+    pkg-name = "true-name";
+    haskell-overlay = hfinal: hprev: {
+      ${pkg-name} = hfinal.callCabal2nix pkg-name ./. {};
+    };
+
+    overlay = final: prev: {
+      haskellPackages = prev.haskellPackages.extend haskell-overlay;
+    };
+  in
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [overlay];
+      };
+
+      hspkgs = pkgs.haskellPackages;
+    in {
+      packages = {
+        true-name = hspkgs.${pkg-name};
+        default = hspkgs.${pkg-name};
+      };
+
+      overlays = {
+        default = overlay;
+      };
+
+      devShell = hspkgs.shellFor {
+        packages = p: [p.${pkg-name}];
+        root = ./.;
+        withHoogle = true;
+        buildInputs = with hspkgs; [
+          haskell-language-server
+          cabal-install
+        ];
+      };
+
+      formatter = pkgs.alejandra;
+    });
+}
